e20cd8bcc44d0032e47f5e0fd55a4ee74370f576
dexmexter <dexter.volkman@gmail.com>
2019-01-20 03:00:27 +0000

    Solve ch9 ex7

--- ch9/ex7.py
+++ ch9/ex7.py
@@ -1,15 +1,32 @@
+import re
 """
 Give me a word with three consecutive double letters.
 """
-def tripledoubles(word):
-    pass
+def tripdoub(word):
+    for i in range(len(word)):
+        if word[i] == word[i + 1] and \
+        word[i + 2] == word[i + 3] and \
+        word[i + 4] == word[i + 5]:
+            return True
+    return False
+
+def regtripdoub(word):
+    regex = r"((.)\2){3}"
+    if re.search(regex, word):
+        return True
+    return False
+
+#print(regtripdoub('Thhsaakkdkje'))
+
 
 def ex7():    
     count = 0
     fin = open('words.txt')
 
     for line in fin:
-        if tripledoubles(line):
+        if regtripdoub(line):
             print(line)
             count += 1
-    print(count)
+    return count
+
+print(ex7())
